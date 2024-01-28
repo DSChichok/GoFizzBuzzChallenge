6a1d1429650649111b1ec0b2b455b945463b2306
DSChichok <47869653+DSChichok@users.noreply.github.com>
2024-01-27 20:54:28 -0800

    Update fizzBuzzChallenge.go 2

--- fizzBuzzChallenge.go
+++ fizzBuzzChallenge.go
@@ -6,10 +6,12 @@ package main
 import ("fmt")
 
 func main(){
+    // Variable declarations
     var fizzy = 3
     var buzzy = 5
-    
     var i = 0
+
+    // Logic
     for i = 0; i <= 100; i++{
         if i % fizzy == 0 && i % buzzy == 0 {
             fmt.Println("FizzBuzz")
